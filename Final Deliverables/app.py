37ff40fd08ed43e1f5ab3cffa05f553d12daee9e
Sandesh-Asokan <mailtosandeshasokan@gmail.com>
2022-11-14 20:51:08 +0530

    Successful Run #1

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 import requests
+import json
 
 
-API_KEY = "dAkQTmsJ7sfRzutZ8fTcNbHZvKD_ZyoxqjtYF7h8VwC7"
+API_KEY = "0zRs_HVV9HNSSzHy-sO_y_XWgsYQFlchHxmeakje1g0H"
 token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
 mltoken = token_response.json()["access_token"]
 header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
@@ -27,12 +28,13 @@ def predict():
     research = int(request.form['research'])
     array_of_input_fields = ['greScore', 'toeflScore', 'univRank', 'sop', 'lor', 'cgpa', 'research']
     array_of_values_to_be_scored = [greScore, toeflScore, univRank, sop, lor, cgpa, research]
-    payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored]}]}
-    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/9f4939ed-7f21-4881-8ae4-234e7515f65a/predictions?version=2022-10-21', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    payload_scoring = {"input_data": [{"field": [array_of_input_fields], "values": [array_of_values_to_be_scored]}]}
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/3b55c9e9-2973-429f-b8e0-d69b19f4ede6/predictions?version=2022-11-12', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
     predictions = response_scoring.json()
     prediction = predictions['predictions'][0]['values'][0][0]
     
-    if prediction:
+
+    if (int(prediction[0]*100)>0):
         return render_template('chance.html')
     else:
         return render_template('noChance.html')
